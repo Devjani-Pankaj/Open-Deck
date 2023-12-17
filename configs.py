61dab70bf05f1e0573671889deee850bcac7d5bd
Pankaj <pankajdevjani260@gmail.com>
2023-12-17 15:16:19 +0530

    open_deck

---
+++ configs.py
@@ -0,0 +1,3 @@
+#PROJECT_PATH = "C:\\Internships\\Silver Touch\\Project\\Study material Recommendation Engine"
+PROJECT_PATH="C:\\Users\\panka\\Downloads\\Courses-Recommendation-system-master\\Courses-Recommendation-system-master"
+DATA_PATH = "./data/features_sample.csv"
